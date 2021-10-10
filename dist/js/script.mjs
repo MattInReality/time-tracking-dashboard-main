27ba164fa956d8b203a3058c987ef80f2ce21013
MattInReality <mattnorth84@gmail.com>
2021-10-10 21:33:18 +0100

    Built out the JS functionality. It feels messy. I chose to dynamically call for and filter the data on each change... but then put the data in a closure so it would only call fetch once. There are better solutions.

---
+++ dist/js/script.mjs
@@ -0,0 +1,82 @@
+const radio = document.querySelector('.time-frame-menu')
+const timers = document.querySelectorAll(".card-timer")
+
+
+// Replace this function with anything that will return the same data structure - could be an api (YAGNI)
+async function getData() {
+    const headers = new Headers()
+    headers.append('Accept', 'application/json')
+    const file = await fetch('data/data.json', {headers})
+    return file.json()
+
+}
+
+// Closure for the data from from getData() if the data is static (YAGNI).
+const data = (() => {
+    let d
+    return async function() {
+        if (!d) {
+          d = await getData()
+        }
+        return d
+    }
+})()
+
+function setTimerData(timerEl, data, timeFrame) {
+    const currentTimer = timerEl.querySelector('.current-timer')
+    const previousTimer = timerEl.querySelector('.previous-timer')
+    const dataItem = data.filter(d => d.title.toLowerCase() === timerEl.dataset.name)[0]
+    const current = dataItem.timeframes[timeFrame.toString()].current
+    const previous = dataItem.timeframes[timeFrame.toString()].previous
+    const previousPrefix = {
+        daily: 'Yesterday',
+        weekly: 'Last Week',
+        monthly: 'Last Month'
+    }
+
+    currentTimer.innerText = current + 'hrs'
+    previousTimer.innerText = `${previousPrefix[timeFrame]} ${previous} hrs`
+
+
+}
+
+async function setTimerCards(timeFrame) {
+    try {
+    const tf = timeFrame || radio.querySelector('input[checked]').value
+    const d = await data()
+    timers.forEach((e)=> {
+        setTimerData(e, d, tf)
+    })
+    } catch(e) {
+        console.log(e)
+    }
+}
+
+(async function init() {
+    await setTimerCards()
+    radio.reset()
+
+})()
+
+radio.addEventListener('input', async (e) => {{
+    e.preventDefault()
+    await setTimerCards(e.target.value)
+}})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
